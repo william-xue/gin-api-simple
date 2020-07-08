b84b538f51ab7a9c0b89fa06acbffe7c444c3450
薛源 <xueyuan@liangteam.com>
2020-07-08 18:16:34 +0800

    use

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"gin-blog/middleware/jwt"
 	"gin-blog/pkg/setting"
 	"gin-blog/routers/api"
 	v1 "gin-blog/routers/api/v1"
@@ -20,6 +21,7 @@ func InitRouter() *gin.Engine {
 	r.GET("/auth", api.GetAuth)
 
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
